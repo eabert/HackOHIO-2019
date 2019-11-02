fd7410e33a3428d270a4b0deceaff5c9ef830b31
Cole Smith <cole.f.smith1@gmail.com>
2019-11-02 13:36:10 -0400

    More infrastructure stuff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 import datetime as dt
 import math
 import requests
@@ -12,12 +13,17 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
+# Load data
+# with open('data/osu_buildings.json') as f:
+#     buildings = json.load(f.read())
+
 navbar = dbc.Container()
 
 body = dbc.Container(
     html.H1("Hack OHI/O 2019"),
-    dcc.Input(id='test_input', value='Yes')
-    dcc.Graph(id='building_map')
+    dbc.Spinner()
+    #dcc.Input(id='test_input', value='Yes'),
+    #dcc.Graph(id='building_map')
 )
 
 app = dash.Dash(__name__)
@@ -25,10 +31,29 @@ server = app.server
 
 app.layout = html.Div([navbar, body])
 
-@app.callback(
-    Output('building_map'),
-    Input('test_input')
-)
+# @app.callback(
+#     Output('building_map', 'figure'),
+#     Input('test_input', 'value')
+# )
+# def make_building_map(test_input):
+#     traces = []
+#     for well_type, dfff in dff.groupby('Well_Type'):
+#         trace = dict(
+#             type='scattermapbox',
+#             lon=dfff['Surface_Longitude'],
+#             lat=dfff['Surface_latitude'],
+#             text=dfff['Well_Name'],
+#             customdata=dfff['API_WellNo'],
+#             name=WELL_TYPES[well_type],
+#             marker=dict(
+#                 size=4,
+#                 opacity=0.6,
+#             )
+#         )
+#         traces.append(trace)
+# 
+    # figure = dict(data=traces, layout=layout)
+    # return figure
 
 # Main
 if __name__ == '__main__':
